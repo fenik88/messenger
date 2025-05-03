69d1ec30d56c8c6834f1865f1704c88f7f82a5da
fenikhey88 <limonprog@gmail.com>
2025-05-04 01:15:36 +0300

    start

---
+++ messenger/messenger/messengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  messengerApp.swift
+//  messenger
+//
+//  Created by Fenik Hey on 04.05.2025.
+//
+
+import SwiftUI
+
+@main
+struct messengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ messenger/messengerTests/messengerTests.swift
@@ -0,0 +1,17 @@
+//
+//  messengerTests.swift
+//  messengerTests
+//
+//  Created by Fenik Hey on 04.05.2025.
+//
+
+import Testing
+@testable import messenger
+
+struct messengerTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
